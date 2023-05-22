39b910708bcd1e011fa2cdd2b5a0b1e59279d17b
humayunrasheed <humayun.51959@gmial.com>
2023-05-23 00:25:20 +0530

    Created playRound

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+function playRound(pS , cS){
+    if( (pS==="Rock" && cS==="Rock" )|| (pS==='Paper' && cS==="Paper")||(pS==="Scissors"&&cS==="Scissors")){
+        return(`Tie ${pS} to ${cS}`);
+    }
+    else if(pS==="Rock" && cS==="Scissors" ){
+        return(`You Won! ${pS} beats ${cS}`);
+    }
+    else if(pS==="Paper" && cS==="Rock" ){
+        return(`You Won! ${pS} beats ${cS}`);
+    }
+    else if(pS==="Scissors" && cS==="Paper" ){
+        return(`You Won! ${pS} beats ${cS}`);
+    }
+    else if(pS==="Rock" && cS==="Paper" ){
+        return(`You Lose! ${cS} beats ${pS}`);
+    }
+    else if(pS==="Paper" && cS==="Scissors" ){
+        return(`You Lose! ${cS} beats ${pS}`);
+    }
+    else if(pS==="Scissors" && cS==="Rock" ){
+        return(`You Lose! ${cS} beats ${pS}`);
+    }
+}
+
+let pS="Rock";
+let cS=getComputerChoice();
